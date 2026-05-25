90f44f4defbf701739ea7133cc597289c9e995e1
Piyush Govindani <govindanipiyush10@gmail.com>
2026-05-25 14:45:48 +0530

    V1

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${inter.variable} ${mukta.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans">
+      <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>
         <InvoiceProvider>{children}</InvoiceProvider>
       </body>
     </html>
